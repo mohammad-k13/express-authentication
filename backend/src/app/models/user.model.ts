310997f895ee89eeca16098c5d8294f6ca26a1cd
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2024-12-07 20:31:24 +0330

    Add Login logic on backend without token

--- backend/src/app/models/user.model.ts
+++ backend/src/app/models/user.model.ts
@@ -1,47 +1,47 @@
-import mongoose from 'mongoose'; 
-import bcrypt from 'bcryptjs'
-
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import IUser from '../types/user.types';
 
 // Define the user schema
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+const userSchema = new mongoose.Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Hash the password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+userSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 // Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword: string) {
-    return await bcrypt.compare(candidatePassword, this.password);
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
 // Create the User model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<IUser>('User', userSchema);
 
 export default User;
