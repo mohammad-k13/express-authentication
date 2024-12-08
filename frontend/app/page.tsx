898936b039d3550d508bb1786cd08d8fb7e91a2b
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2024-12-08 18:23:39 +0330

    fronedn - add tabs to homepage to show two forms(register & login)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,57 @@
-import { LoginForm } from "@/components/forms/login-form";
-import { RegisterFrom } from "@/components/forms/register-form";
-import React from "react";
+import { LoginForm } from "@/components/forms/login-form"
+import { RegisterFrom } from "@/components/forms/register-form"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 const Home = () => {
-    return (
-        <>
-            <section className="w-[500px] h-fit border-[1px] p-10 rounded-md">
-                <RegisterFrom />
-            </section>
-            <section className="w-[500px] h-fit border-[1px] p-10 rounded-md">
-                <LoginForm />
-            </section>
-        </>
-    );
-};
-
+  return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Register</TabsTrigger>
+        <TabsTrigger value="password">Login</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Register</CardTitle>
+            <CardDescription>
+              Create You Account to export more!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <RegisterFrom />
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Login To Your Account to see information
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+           <LoginForm />
+           </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  )
+}
 export default Home;
