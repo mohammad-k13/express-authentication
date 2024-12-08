a9c28e91736442f8a4e0103f8dab34d3729ec7b6
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2024-12-08 18:35:15 +0330

    fix a ui bug and add some log to register form to check username is unique

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -20,38 +20,38 @@ import {
 
 const Home = () => {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Register</TabsTrigger>
-        <TabsTrigger value="password">Login</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Register</CardTitle>
-            <CardDescription>
-              Create You Account to export more!
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <RegisterFrom />
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>
-              Login To Your Account to see information
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-           <LoginForm />
-           </CardContent>
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <section className="w-full h-screen flex items-center justify-center"><Tabs defaultValue="account" className="w-[400px] h-[600px] flex items-center justify-start flex-col">
+    <TabsList className="grid w-full grid-cols-2">
+      <TabsTrigger value="account">Register</TabsTrigger>
+      <TabsTrigger value="password">Login</TabsTrigger>
+    </TabsList>
+    <TabsContent value="account" className="w-full">
+      <Card>
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>
+            Create You Account to export more!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <RegisterFrom />
+        </CardContent>
+      </Card>
+    </TabsContent>
+    <TabsContent value="password" className="w-full">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Login To Your Account to see information
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+         <LoginForm />
+         </CardContent>
+      </Card>
+    </TabsContent>
+  </Tabs></section>
   )
 }
 export default Home;
